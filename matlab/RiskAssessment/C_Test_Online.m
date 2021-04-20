c4e2fd5caccc45d4e49656ddcbf56e13c1513103
Kourosh <kourosh.darvish@gmail.com>
2021-04-20 13:57:34 +0200

    fix the communication of the data for online risk assessment

--- matlab/RiskAssessment/C_Test_Online.m
+++ matlab/RiskAssessment/C_Test_Online.m
@@ -2,9 +2,16 @@ clc; clear; close all;
 %% Parameters
 load('Nets/net1.mat')
 userMass=80; %kg
+userWeight=userMass *9.81;
 sleepTime = 0.01; % sec
 
+NoFeatures= 60;
+NoFinalSamples = 50;
 
+humanKinDynTimeSeries=[]; % all the time series data
+humanKinDynVector=zeros(1,NoFeatures); % only the last vector
+scalingVector=ones(1,NoFeatures);
+scalingVector(1,end-11:end)=1/userWeight; % end-11:end ==> related to the external wrenches
 
 %% Initialize yarp Network
 yarp.Network.init()
@@ -28,52 +35,77 @@ risk_port.open('/estimatedRisk:o');
 human_kin_dynBottle= yarp.Bottle;
 liftingStartedBottle= yarp.Bottle;
 liftingEndedBottle= yarp.Bottle;
-liftingStarted = true;
+liftingStarted = false;
 liftingEnded = false;
 
 
-
 while true
-    human_kin_dynBottle = human_kin_dyn_port.read(false);
-    liftingStartedBottle = lifting_started_port.read(false);
-    liftingEndedBottle = lifting_ended_port.read(false);
     
-    %     if isequal(liftingStartedBottle,[])
-    if sum(size(liftingStartedBottle))==0
-        liftingStarted = false;
-    else
-        disp(liftingStartedBottle.toString());
+    NoInfo = lifting_started_port.getPendingReads();
+    if NoInfo >0
+        liftingStartedBottle = lifting_started_port.read(false);
+        liftingStartedInfo= liftingStartedBottle.toString();
+        disp(liftingStartedInfo)
+        
         liftingStarted = liftingStartedBottle.get(0).asBool();
         if liftingStarted
             disp('lifting is started: collecting data ...' )
         end
     end
     
-    
-    %     if isequal(liftingEndedBottle, [])
-    if sum(size(liftingEndedBottle))==0
-        liftingEnded = false;
-    else
-        disp(liftingEndedBottle.toString());
+    NoInfo = lifting_ended_port.getPendingReads();
+    if NoInfo >0
+        liftingEndedBottle = lifting_ended_port.read(false);
+        liftingEndedInfo= liftingEndedBottle.toString();
+        disp(liftingEndedInfo)
+        
         liftingEnded = liftingEndedBottle.get(0).asBool();
         if liftingEnded
             disp('lifting is ended: estimaing the risk ...' )
+            break;
         end
     end
+    
+    %     kinDynInfo= human_kin_dynBottle.toString();
+    %     disp(kinDynInfo)
+    
+    
     
     if liftingStarted && ~liftingEnded
-        %         if isequal(human_kin_dyn, [])
-        if sum(size(human_kin_dynBottle))==0
-            disp('variable does  not exist');
-        else
+        NoInfo = human_kin_dyn_port.getPendingReads();
+        if NoInfo>0
+            human_kin_dynBottle = human_kin_dyn_port.read(false);
+            
             kinDynInfo= human_kin_dynBottle.toString();
             disp(kinDynInfo);
+            
+            % get the information from the yarp port
+            for i=1:NoFeatures
+                humanKinDynVector(1,i)=human_kin_dynBottle.get(i-1).asFloat64() * scalingVector(1,i);
+            end
+            
+            
+            humanKinDynTimeSeries=[humanKinDynTimeSeries;humanKinDynVector]; % all the time series data
+            
         end
     end
-    
     pause(sleepTime)
 end
 
+
+disp('human time series data');
+disp(humanKinDynTimeSeries);
+
+estimatedRisk=2;
+
+riskVector = risk_port.prepare();
+riskVector.clear();
+
+riskVector.push_back(estimatedRisk)
+risk_port.write();
+
+disp('estimation is done; closing ...')
+pause(1.0);
 
 risk_port.close();
 yarp.Network.fini();
