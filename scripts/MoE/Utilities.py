d6b9867082be7ccc3e38ec828338adcc6bb61a5b
Kourosh <kourosh.darvish@gmail.com>
2021-09-08 10:29:26 +0200

    add the MoE first implementation, updated the data pre-processing, added the weight normalization for the human wrench related info

---
+++ scripts/MoE/Utilities.py
@@ -0,0 +1,290 @@
+from fnmatch import filter
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.utils import plot_model
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+from pathlib import Path
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.metrics import Accuracy, CategoricalAccuracy
+from tensorflow.keras.layers import Lambda, Dense, Flatten, LSTM, Reshape, Dropout, BatchNormalization, Conv1D, MaxPooling1D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+
+def get_dense_model(number_categories, input_shape, reg_l2, dp_rate):
+    model = Sequential([
+        # Take the last time step.
+        # Shape [batch, time, features] => [batch, 1, features]
+        # Lambda(lambda x: x[:, -1:, :]),
+        Flatten(input_shape=input_shape),
+        # Shape => [batch, 1, dense_units]
+        # tf.keras.layers.Dense(512, activation='relu'),
+        # tf.keras.layers.Dense(256, activation='relu'),
+        Dense(512, activation='relu', kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Dense(128, activation='relu', kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Dense(64, activation='relu', kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Dense(32, activation='relu', kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Dense(16, activation='relu', kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Dense(number_categories, activation='softmax')
+    ])
+    return model
+
+
+def get_cnn_model(number_categories, input_shape, reg_l2, dp_rate):
+    model = Sequential([
+        # Shape [batch, time, features] => [batch, CONV_WIDTH, features]
+        Conv1D(filters=128, kernel_size=3, activation='relu', padding='same',
+               kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2),
+               input_shape=input_shape),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Conv1D(filters=64, kernel_size=3, activation='relu', padding='same',
+               kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Conv1D(filters=32, kernel_size=3, activation='relu', padding='same',
+               kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Conv1D(filters=16, kernel_size=3, activation='relu', padding='same',
+               kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Flatten(),
+        Dense(32, activation='relu', kernel_regularizer=regularizers.l2(reg_l2),
+              bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Dense(16, activation='relu', kernel_regularizer=regularizers.l2(reg_l2),
+              bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Dense(number_categories, activation='softmax')
+    ])
+    return model
+
+
+def get_lstm_model(number_categories, input_shape, reg_l2, dp_rate):
+    model = Sequential([
+        LSTM(128, return_sequences=True,  activation='relu', name='lstm_a',
+             kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2),
+             recurrent_regularizer=regularizers.l2(reg_l2), dropout=dp_rate, recurrent_dropout=dp_rate,
+             input_shape=(input_shape[0], input_shape[1])),
+        BatchNormalization(),
+        LSTM(64, return_sequences=True, activation='relu', name='lstm_b',
+             kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2),
+             recurrent_regularizer=regularizers.l2(reg_l2), dropout=dp_rate, recurrent_dropout=dp_rate),
+        BatchNormalization(),
+        LSTM(32, return_sequences=True, activation='relu', name='lstm_c',
+             kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2),
+             recurrent_regularizer=regularizers.l2(reg_l2), dropout=dp_rate, recurrent_dropout=dp_rate),
+        BatchNormalization(),
+        LSTM(16, return_sequences=True, activation='relu', name='lstm_d',
+             kernel_regularizer=regularizers.l2(reg_l2), bias_regularizer=regularizers.l2(reg_l2),
+             recurrent_regularizer=regularizers.l2(reg_l2), dropout=dp_rate, recurrent_dropout=dp_rate),
+        Flatten(),
+        Dense(32, activation='relu', kernel_regularizer=regularizers.l2(reg_l2),
+              bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Dense(16, activation='relu', kernel_regularizer=regularizers.l2(reg_l2),
+              bias_regularizer=regularizers.l2(reg_l2)),
+        BatchNormalization(),
+        Dropout(dp_rate),
+        Dense(number_categories, activation='softmax')
+    ])
+    return model
+
+
+def compile_model(model):
+    model.compile(loss=CategoricalCrossentropy(from_logits=False),
+                  optimizer=Adam(),
+                  metrics=['accuracy'])
+    # To help the numerical stability of the back propagation, I can remove the softmax from the last dense layer and
+    # add from_logits=True to the loss function
+
+    return model
+
+
+def fit_model(model, window, patience=2, max_epochs=20, model_path='', model_name='myModel'):
+
+    early_stopping = EarlyStopping(monitor='val_loss',
+                                   patience=patience,
+                                   min_delta=0.01,
+                                   mode='min')
+    callback_loss_accuracy_plot = CallbackPlotLossesAccuracy()
+
+    checkpoint_best = ModelCheckpoint(filepath='{}/{}.h5'.format(model_path, model_name),
+                                      save_weights_only=False,
+                                      save_freq='epoch',
+                                      monitor='val_accuracy',
+                                      save_best_only=True,
+                                      verbose=1)
+
+    history = model.fit(window.train,
+                        epochs=max_epochs,
+                        validation_data=window.val,
+                        callbacks=[early_stopping, callback_loss_accuracy_plot, checkpoint_best])
+    return history
+
+
+def plot_losses(history):
+    plt.figure(figsize=(12, 8))
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+    return
+
+
+def plot_accuracy(history):
+    plt.figure(figsize=(12, 8))
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+    return
+
+def compile_and_fit(model, window, plot_losses, ):
+    early_stopping = EarlyStopping(monitor='val_loss',
+                                   patience=patience,
+                                   mode='min')
+
+
+    # model.compile(loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+    #               optimizer=tf.optimizers.Adam(),
+    #               metrics=[tf.metrics.Accuracy()])
+
+
+    history = model.fit(window.train, epochs=MAX_EPOCHS,
+                        validation_data=window.val
+                        , callbacks=[early_stopping
+                            #, plot_losses
+                            ],
+                        use_multiprocessing=True)
+
+
+
+    return history
+
+
+def save_nn_model(model, file_path='', file_name='myModel'):
+    Path(file_path).mkdir(parents=True, exist_ok=True)
+    model.save('{}/{}.h5'.format(file_path, file_name))  # creates a HDF5 file 'my_model.h5'
+    print('the model is saved as: {}'.format('{}/{}.h5'.format(file_path, file_name)))
+    return
+
+def visualize_model(model, file_path='', file_name='myModel'):
+    plot_model(model, to_file='{}/{}.png'.format(file_path, file_name), show_shapes=True)
+
+    return
+
+
+def load_model_from_file(file_path='', file_name='myModel'):
+    model = load_model('{}/{}.h5'.format(file_path, file_name))
+    return model
+
+
+def translate_metric(x):
+    translations = {'acc': "Accuracy", 'loss': "Log-loss (cost function)"}
+    if x in translations:
+        return translations[x]
+    else:
+        return x
+
+
+class CallbackPlotLossesAccuracy(tf.keras.callbacks.Callback):
+    def __init__(self, figsize=None, file_path='', file_name='myModel'):
+        # super(PlotLosses, self).__init__()
+        self.plot_loss = plt.figure(1, figsize=(12, 8))
+        self.plot_acc = plt.figure(2, figsize=(12, 8))
+        # self.file_path = file_path
+        # self.file_name = file_name
+
+    # def on_train_begin(self, logs=None):
+    #     print('self.params', self.params)
+    #     self.base_metrics = [metric for metric in self.params['metrics'] if not metric.startswith('val_')]
+        self.losses = []
+        self.val_losses = []
+        self.accuracy = []
+        self.val_accuracy = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        print('epoch: {} , loss: {} , val_loss : {}'.format(epoch, logs['loss'], logs['val_loss']))
+        self.losses.append(logs['loss'])
+        self.val_losses.append(logs['val_loss'])
+        self.accuracy.append(logs['accuracy'])
+        self.val_accuracy.append(logs['val_accuracy'])
+
+        plt.figure(1, figsize=(12, 8))
+        self.plot_loss.clf()
+        plt.plot(self.losses)
+        plt.plot(self.val_losses)
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+        plt.pause(0.001)
+        plt.tight_layout()
+
+        plt.figure(2, figsize=(12, 8))
+        self.plot_acc.clf()
+        plt.plot(self.accuracy)
+        plt.plot(self.val_accuracy)
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'validation'], loc='upper left')
+        plt.show()
+        plt.pause(0.001)
+        plt.tight_layout()
+
+
+        # save the model
+
+        # self.save_model(epoch, val_loss=logs['val_loss'])
+
+        # for metric_id, metric in enumerate(self.base_metrics):
+        #     if metric == 'loss':
+        #
+        #         plt.plot(range(1, len(self.logs) + 1),
+        #                  [log[metric] for log in self.logs],
+        #                  label="training")
+        #         if self.params['do_validation']:
+        #             plt.plot(range(1, len(self.logs) + 1),
+        #                      [log['val_' + metric] for log in self.logs], '--',
+        #                      label="validation")
+        #         plt.title(translate_metric(metric))
+        #         plt.xlabel('epoch')
+        #         plt.legend(loc='center right')
+        #
+
+
+
+    def save_model(self, epoch, val_loss):
+        # creates a HDF5 file 'my_model_epochNumber_valLoss.h5'
+        Path(self.file_path).mkdir(parents=True, exist_ok=True)
+        self.model.save('{}/{}_{}_{}.h5'.format(self.file_path, self.file_name, epoch, val_loss))
+        return
+
---
+++ scripts/MoE/main_train_moe.py
@@ -0,0 +1,256 @@
+import os
+import datetime
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import copy
+
+# functions and classes to import
+from WindowGeneratorMoE import WindowGeneratorMoE
+from Utilities import get_dense_model, get_cnn_model, get_lstm_model, compile_model, fit_model, \
+    plot_losses, plot_accuracy
+from Utilities import save_nn_model
+
+# def main():
+if __name__ == "__main__":
+
+    # =====================
+    # == CONFIGURATIONS ==
+    # =====================
+
+    # high level flags for training
+    learn_dense_model = False
+    learn_cnn_model = False
+    learn_lstm_model = True
+    do_performance_analysis = True
+    normalize_input = True
+    output_categorical = True
+    save_model = True
+    verbose = False
+
+    # configurations for the datasets
+    model_name = 'model'
+    models_path = 'models'
+    data_path = '/home/kourosh/icub_ws/external/DataSet/' \
+                'HumanDataForActionMotionPrediction/ActionRecognition/' \
+                'carefulAnnotation/2/Dataset_2021_08_19_20_06_39.txt'
+    user_mass = 79.0
+
+    output_steps = 1  # ! at the moment only the value `1` is possible
+    shift = 1  # ! offset
+    input_width = 15  # ! default: 10
+    patience = 10  # ! default: 4
+    max_subplots = 5
+    train_percentage = 0.7  # ! the percentage of of the time series data from starting  for training
+    val_percentage = 0.2    # ! the percentage of the time series data after training data for validation
+    test_percentage = 1.0 - (train_percentage + val_percentage)  # ! the amount of data at end for testing
+
+    # general configurations for the neural networks
+    regularization_l2 = 1e-3
+    dropout_rate = 0.3
+    max_epochs = 100  # Default: 20
+
+    # =====================
+    # ====== DATASET ======
+    # =====================
+
+    input_feature_list = []  # ! if empty all the columns of the dataset will be added to the dataset data frame
+    # ! Examples:
+    # !         features_list = ['jLeftKnee_roty_val', 'jRightKnee_roty_val']
+    # !         input_feature_list = ['temperature']
+    output_feature_list = ['label']  # ! the title of the column for the output dataset
+    pop_list = ['time', 'label']  # ! pop list used for only input features list
+    df_raw = pd.read_csv(data_path, sep=' ')  # ! raw data frame
+
+    if input_feature_list:  # ! define the input feature list
+        df_input = df_raw[input_feature_list]
+    else:
+        df_input = df_raw
+
+    if output_feature_list:  # ! define the output feature list
+        df_output = df_raw[output_feature_list]
+    else:
+        df_output = df_raw
+
+    if output_categorical:  # ! get the output label in case of categorical ouputs
+        df_output = pd.get_dummies(df_output)
+        output_labels = df_output.keys()
+        number_categories = len(output_labels)  # ! the number of categories
+        print('output labels : {}'.format(output_labels))
+
+    # ! start the time from the zero, depends on the application
+    if 'time' in df_raw:
+        df_time = df_raw['time'] - df_raw['time'][0]
+    df_input.head()
+
+    if pop_list is not None:
+        for pop_name in pop_list:
+            if pop_name in df_input:
+                df_input.pop(pop_name)
+
+    if verbose:
+        df_input.describe().transpose()
+
+    # ! normalize the force/torque values with the user weight
+    wrench_keys = [key for key in df_input.keys() if 'shoe' in key.lower()]
+    gravity = 9.81
+    df_input_weight_normalized = df_input
+    for key in wrench_keys:
+        df_input_weight_normalized[key] = df_input[key]/(user_mass * gravity)
+
+    # ! divide the data to training, validation, test sets
+    n = len(df_input_weight_normalized)
+    train_input_df = df_input_weight_normalized[0:int(n * train_percentage)]
+    val_input_df = df_input_weight_normalized[int(n * train_percentage):int(n * (train_percentage + val_percentage))]
+    test_input_df = df_input_weight_normalized[int(n * (train_percentage + val_percentage)):]
+
+    train_target_df = df_output[0:int(n * train_percentage)]
+    val_target_df = df_output[int(n * train_percentage):int(n * (train_percentage + val_percentage))]
+    test_target_df = df_output[int(n * (train_percentage + val_percentage)):]
+
+    # ! normalize all the data based on the training data
+    if normalize_input:
+        train_input_mean = train_input_df.mean()
+        train_input_std = train_input_df.std()
+
+        train_input_df = (train_input_df - train_input_mean) / train_input_std
+        val_input_df = (val_input_df - train_input_mean) / train_input_std
+        test_input_df = (test_input_df - train_input_mean) / train_input_std
+
+        # ! check: maybe I can delete it.
+        df_std = (df_input_weight_normalized - train_input_mean) / train_input_std
+        df_std = df_std.melt(var_name='Column', value_name='Normalized')
+
+    multi_window = WindowGeneratorMoE(input_width=input_width,
+                                      label_width=output_steps,
+                                      shift=shift,
+                                      train_input_df=train_input_df, val_input_df=val_input_df,
+                                      test_input_df=test_input_df,
+                                      train_target_df=train_target_df, val_target_df=val_target_df,
+                                      test_target_df=test_target_df)
+
+    multi_window.train
+    print('02: output_labels', output_labels)
+    if verbose:
+        multi_window.plot(max_subplots=3, output_labels=output_labels)
+
+    multi_window_cpy = copy.deepcopy(multi_window)
+
+    input_data_example, __ = multi_window_cpy.example
+    input_shape = (input_data_example.shape[1], input_data_example.shape[2])
+    multi_val_performance = {}
+    multi_performance = {}
+
+    # DENSE
+
+    if learn_dense_model:
+
+        model_dense = get_dense_model(number_categories, input_shape, regularization_l2, dropout_rate)
+        model_dense = compile_model(model_dense)
+        history_dense = fit_model(model_dense,
+                                  multi_window_cpy,
+                                  patience,
+                                  max_epochs,
+                                  model_path=models_path,
+                                  model_name=model_name + '_Dense_Best')
+        plot_losses(history_dense)
+        plot_accuracy(history_dense)
+
+        # history = compile_and_fit(multi_dense_model, multi_window_cpy, plot_losses=plot_losses,
+        #                       patience=PATIENCE, MAX_EPOCHS=MAX_EPOCHS)
+
+        multi_val_performance['Dense'] = model_dense.evaluate(multi_window_cpy.val)
+        multi_performance['Dense'] = model_dense.evaluate(multi_window_cpy.test, verbose=0)
+        if verbose:
+            multi_window.plot(model_dense, max_subplots=3, output_labels=output_labels)
+
+    # ## CONV
+    if learn_cnn_model:
+        # multi_conv_model = tf.keras.Sequential([
+        #     # Shape [batch, time, features] => [batch, CONV_WIDTH, features]
+        #     tf.keras.layers.Lambda(lambda x: x[:, -CONV_WIDTH:, :]),
+        #     # Shape => [batch, 1, conv_units]
+        #     tf.keras.layers.Conv1D(HIDDEN_LAYER_SIZE, activation='relu', kernel_size=(CONV_WIDTH), input_shape=()),
+        #     # Shape => [batch, 1,  out_steps*features]
+        #     tf.keras.layers.Dense(OUT_STEPS * NUMBER_CATEGORIES,
+        #                           kernel_initializer=tf.initializers.zeros(), activation='softmax'),
+        #     # Shape => [batch, out_steps, features]
+        #     tf.keras.layers.Reshape([OUT_STEPS*NUMBER_CATEGORIES])
+        # ])
+        # multi_conv_model._name = 'multi_conv_model'
+        #
+        # history = compile_and_fit(multi_conv_model, multi_window_cpy, plot_losses=plot_losses,
+        #                           patience=PATIENCE, MAX_EPOCHS=MAX_EPOCHS)
+        model_cnn = get_cnn_model(number_categories, input_shape, regularization_l2, dropout_rate)
+        model_cnn.summary()
+        model_cnn = compile_model(model_cnn)
+        history_cnn = fit_model(model_cnn,
+                                multi_window_cpy,
+                                patience,
+                                max_epochs,
+                                model_path=models_path,
+                                model_name=model_name + '_CNN_Best')
+        plot_losses(history_cnn)
+        plot_accuracy(history_cnn)
+
+        multi_val_performance['Conv'] = model_cnn.evaluate(multi_window_cpy.val)
+        multi_performance['Conv'] = model_cnn.evaluate(multi_window_cpy.test, verbose=0)
+        if verbose:
+            multi_window.plot(model_cnn, max_subplots=max_subplots, output_labels=output_labels)
+
+    # RNN
+    if learn_lstm_model:
+        model_lstm = get_lstm_model(number_categories, input_shape, regularization_l2, dropout_rate)
+        model_lstm.summary()
+        model_lstm = compile_model(model_lstm)
+        history_lstm = fit_model(model_lstm,
+                                 multi_window_cpy,
+                                 patience,
+                                 max_epochs,
+                                 model_path=models_path,
+                                 model_name=model_name + '_LSTM_Best')
+        plot_losses(history_lstm)
+        plot_accuracy(history_lstm)
+
+        multi_val_performance['LSTM'] = model_lstm.evaluate(multi_window_cpy.val)
+        multi_performance['LSTM'] = model_lstm.evaluate(multi_window_cpy.test, verbose=0)
+
+        if verbose:
+            multi_window.plot(model_lstm, max_subplots=max_subplots, output_labels=output_labels)
+
+    # performances
+    if do_performance_analysis:
+        x = np.arange(len(multi_performance))
+        width = 0.3
+
+        metric_name = 'accuracy'
+        if learn_lstm_model:
+            metric_index = model_lstm.metrics_names.index('accuracy')
+        elif learn_dense_model:
+            metric_index = model_dense.metrics_names.index('accuracy')
+        elif learn_cnn_model:
+            metric_index = model_cnn.metrics_names.index('accuracy')
+
+        val_mae = [v[metric_index] for v in multi_val_performance.values()]
+        test_mae = [v[metric_index] for v in multi_performance.values()]
+        plt.figure(figsize=(12, 8))
+        plt.bar(x - 0.17, val_mae, width, label='Validation')
+        plt.bar(x + 0.17, test_mae, width, label='Test')
+        plt.xticks(ticks=x, labels=multi_performance.keys(),
+                   rotation=45)
+        plt.ylabel(f'MAE (average over all times and outputs)')
+        _ = plt.legend()
+        for name, value in multi_performance.items():
+            print(f'{name:8s}: {value[1]:0.4f}')
+
+    if save_model:
+        if learn_dense_model:
+            save_nn_model(model_dense, file_path=models_path, file_name=model_name + '_Dense')
+        if learn_cnn_model:
+            save_nn_model(model_cnn, file_path=models_path, file_name=model_name + '_CNN')
+        if learn_lstm_model:
+            save_nn_model(model_lstm, file_path=models_path, file_name=model_name + '_LSTM')
+
+# if __name__ == "__main__":
+#     main()
